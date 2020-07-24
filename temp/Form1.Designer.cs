d15cd5c53ad445aa08c6c5aab65455d6119e376a
kaleemullah <kaleemku9991@gmail.com>
2020-07-25 04:46:21 +0500

    tic tac simple

---
+++ temp/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace temp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.p8 = new System.Windows.Forms.PictureBox();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p7 = new System.Windows.Forms.PictureBox();
+            this.p9 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.p6 = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.p8);
+            this.groupBox1.Controls.Add(this.p5);
+            this.groupBox1.Controls.Add(this.p2);
+            this.groupBox1.Controls.Add(this.p7);
+            this.groupBox1.Controls.Add(this.p9);
+            this.groupBox1.Controls.Add(this.p4);
+            this.groupBox1.Controls.Add(this.p6);
+            this.groupBox1.Controls.Add(this.p1);
+            this.groupBox1.Controls.Add(this.p3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 396);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TIC TAC";
+            // 
+            // p8
+            // 
+            this.p8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p8.Location = new System.Drawing.Point(138, 256);
+            this.p8.Name = "p8";
+            this.p8.Size = new System.Drawing.Size(165, 115);
+            this.p8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p8.TabIndex = 6;
+            this.p8.TabStop = false;
+            this.p8.Click += new System.EventHandler(this.p8_Click);
+            // 
+            // p5
+            // 
+            this.p5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p5.Location = new System.Drawing.Point(138, 136);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(166, 123);
+            this.p5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p5.TabIndex = 6;
+            this.p5.TabStop = false;
+            this.p5.Click += new System.EventHandler(this.p5_Click);
+            // 
+            // p2
+            // 
+            this.p2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p2.Location = new System.Drawing.Point(137, 19);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(166, 119);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p2.TabIndex = 6;
+            this.p2.TabStop = false;
+            this.p2.Click += new System.EventHandler(this.p2_Click);
+            // 
+            // p7
+            // 
+            this.p7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p7.Location = new System.Drawing.Point(0, 256);
+            this.p7.Name = "p7";
+            this.p7.Size = new System.Drawing.Size(142, 115);
+            this.p7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p7.TabIndex = 0;
+            this.p7.TabStop = false;
+            this.p7.Click += new System.EventHandler(this.p7_Click);
+            // 
+            // p9
+            // 
+            this.p9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p9.Location = new System.Drawing.Point(300, 256);
+            this.p9.Name = "p9";
+            this.p9.Size = new System.Drawing.Size(179, 115);
+            this.p9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p9.TabIndex = 5;
+            this.p9.TabStop = false;
+            this.p9.Click += new System.EventHandler(this.p9_Click);
+            // 
+            // p4
+            // 
+            this.p4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p4.Location = new System.Drawing.Point(0, 136);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(142, 123);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p4.TabIndex = 0;
+            this.p4.TabStop = false;
+            this.p4.Click += new System.EventHandler(this.p4_Click);
+            // 
+            // p6
+            // 
+            this.p6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p6.Location = new System.Drawing.Point(300, 136);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(179, 123);
+            this.p6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p6.TabIndex = 5;
+            this.p6.TabStop = false;
+            this.p6.Click += new System.EventHandler(this.p6_Click);
+            // 
+            // p1
+            // 
+            this.p1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p1.Location = new System.Drawing.Point(0, 19);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(142, 119);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p1.TabIndex = 0;
+            this.p1.TabStop = false;
+            this.p1.BackgroundImageChanged += new System.EventHandler(this.p1_Click);
+            this.p1.Click += new System.EventHandler(this.p1_Click);
+            // 
+            // p3
+            // 
+            this.p3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.p3.Location = new System.Drawing.Point(300, 19);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(179, 119);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p3.TabIndex = 5;
+            this.p3.TabStop = false;
+            this.p3.Click += new System.EventHandler(this.p3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Image = global::temp.Properties.Resources.TIC;
+            this.pictureBox1.Location = new System.Drawing.Point(503, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(273, 292);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(522, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "player1_points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(522, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "player2_points:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(659, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(659, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(522, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "TIE_GAMES:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(660, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox p8;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox p7;
+        private System.Windows.Forms.PictureBox p9;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox p6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ temp/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace temp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+
+
+
+
+        public void ChangeColor(PictureBox pic)
+        {
+            if (a % 2 == 0)
+            {
+                a += 1;
+                pic.BackColor = Color.Black;
+            }
+            else
+            {
+                a += 1;
+                pic.BackColor = Color.Red;
+            }
+
+            checkWin();
+            
+        }
+
+        bool a1, a2, a3, a4, a5, a6, a7, a8, a9;
+
+        private void p1_Click(object sender, EventArgs e)
+        {
+
+            if (a1 == false )
+            {
+                ChangeColor(p1);
+                a1 = true;
+            }
+            else
+            {
+                ChangeColor(p1);
+                a1 = false;
+            }
+        }
+
+        private void p2_Click(object sender, EventArgs e)
+        {
+            if (a2 == false)
+            {
+                ChangeColor(p2);
+                a2 = true;
+            }
+            else
+            {
+                ChangeColor(p2);
+                a2 = false;
+            }
+
+        }
+
+        private void p3_Click(object sender, EventArgs e)
+        {
+            if (a3 == false)
+            {
+                ChangeColor(p3);
+                a3 = true;
+            }
+            else
+            {
+                ChangeColor(p3);
+                a3 = false;
+            }
+        }
+
+        private void p4_Click(object sender, EventArgs e)
+        {
+            if (a4 == false)
+            {
+                ChangeColor(p4);
+                a4 = true;
+            }
+            else
+            {
+                ChangeColor(p4);
+                a4 = false;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p5_Click(object sender, EventArgs e)
+        {
+            if (a5 == false)
+            {
+                ChangeColor(p5);
+                a5 = true;
+            }
+            else
+            {
+                ChangeColor(p5);
+                a5 = false;
+            }
+        }
+
+        private void p6_Click(object sender, EventArgs e)
+        {
+            if (a6 == false)
+            {
+                ChangeColor(p6);
+                a6 = true;
+            }
+            else
+            {
+                ChangeColor(p6);
+                a6 = false;
+            }
+
+        }
+
+        private void p7_Click(object sender, EventArgs e)
+        {
+            if (a7 == false)
+            {
+                ChangeColor(p7);
+                a7 = true;
+            }
+            else
+            {
+                ChangeColor(p7);
+                a7 = false;
+            }
+        }
+
+        private void p8_Click(object sender, EventArgs e)
+        {
+            if (a8 == false)
+            {
+                ChangeColor(p8);
+                a8 = true;
+            }
+            else
+            {
+                ChangeColor(p8);
+                a8 = false;
+            }
+
+        }
+
+        private void p9_Click(object sender, EventArgs e)
+        {
+            if (a9 == false)
+            {
+                ChangeColor(p9);
+                a9 = true;
+            }
+            else
+            {
+                ChangeColor(p9);
+                a9 = false;
+            }
+
+        }
+
+
+
+        public void checkWin()
+        {
+
+            if (p1.BackColor == p2.BackColor && p2.BackColor == p3.BackColor && p1.BackColor != SystemColors.Control)
+            {
+
+                WhoWon();
+
+
+
+            }
+            else if (p2.BackColor==p5.BackColor &&p2.BackColor==p8.BackColor&&p2.BackColor!=SystemColors.Control)
+            {
+                WhoWon();
+            }
+
+            else if (p1.BackColor == p4.BackColor && p4.BackColor == p7.BackColor && p1.BackColor != SystemColors.Control)
+            {
+
+                WhoWon();
+
+
+
+            }
+
+            else if (a == 9)
+            {
+
+                MessageBox.Show("Tie");
+                d = d + 1;
+                label6.Text = d.ToString();
+                Nextgame();
+                a = 0;
+            }
+
+
+            else if (p7.BackColor == p8.BackColor && p7.BackColor == p9.BackColor && p7.BackColor != SystemColors.Control)
+            {
+                WhoWon();
+
+            }
+            else if (p4.BackColor==p5.BackColor && p6.BackColor==p4.BackColor && p4.BackColor !=SystemColors.Control)
+            {
+                WhoWon();
+            }
+
+
+
+            else if (p7.BackColor == p5.BackColor && p7.BackColor == p3.BackColor && p7.BackColor != SystemColors.Control)
+            {
+                WhoWon();
+
+            }
+
+
+
+            else if (p9.BackColor == p5.BackColor && p9.BackColor == p1.BackColor && p9.BackColor != SystemColors.Control)
+            {
+                WhoWon();
+            }
+            else if (p9.BackColor == p6.BackColor && p9.BackColor == p3.BackColor && p9.BackColor != SystemColors.Control)
+            {
+                WhoWon();
+            }
+
+        }
+
+        public void WhoWon()
+        {
+            if (a % 2 != 0)
+            {
+                MessageBox.Show("Hurryyy! Player 1 Won");
+
+
+                b = b + 1;
+                label3.Text = b.ToString();
+                Nextgame();
+                a = 0;
+            }
+            else if (a%2==0)
+            {
+                MessageBox.Show("Hurryyy! Player 2 won");
+
+                c = c + 1;
+                label4.Text = c.ToString();
+                Nextgame();
+                a = 0;
+            }
+
+            
+
+
+        }
+
+
+
+        public void Nextgame()
+        {
+            MessageBox.Show("RESTART GAME");
+            
+            p1.BackColor = SystemColors.Control;
+            p2.BackColor = SystemColors.Control;
+            p3.BackColor = SystemColors.Control;
+            p4.BackColor = SystemColors.Control;
+            p5.BackColor = SystemColors.Control;
+            p6.BackColor = SystemColors.Control;
+            p7.BackColor = SystemColors.Control;
+            p8.BackColor = SystemColors.Control;
+            p9.BackColor = SystemColors.Control;
+
+
+        }
+        
+        
+    }
+}
